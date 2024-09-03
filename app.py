f0a887d5b32974f4b2d1cccd8a93772ed9a222d0
Aloysius-See <78715602+Aloysius-See@users.noreply.github.com>
2024-09-03 09:33:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 class Tasks(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    todo = db.Column(db.String(40))
+    todo = db.Column(db.String())
 
     def __init__(self, todo):
         self.todo = todo
@@ -32,9 +32,11 @@ def index():
 @app.route("/add", methods=["POST"])
 def add():
     todo_name = request.form['todo']
-    new_task = Tasks(todo=todo_name)  # Create a new task instance
-    db.session.add(new_task)  # Add the task to the database
-    db.session.commit()  # Commit the transaction
+    # Create a new task instance
+    new_task = Tasks(todo=todo_name)  
+    # Add the task to the database
+    db.session.add(new_task)  
+    db.session.commit()  
     return redirect(url_for("index"))
 
 @app.route("/edit/<int:id>", methods=['GET', 'POST'])
@@ -50,7 +52,7 @@ def edit(id):
 @app.route("/check/<int:id>", methods=['POST'])
 def check(id):
     task = Tasks.query.get_or_404(id)
-    task.done = not task.done  # Toggle the done status
+    # task.done = not task.done  
     db.session.commit()
     return redirect(url_for("index"))
 
